9025fe6271f48eccd164fcd81cab337b9388a400
motahare-M0r <mormotahare@gmal.com>
2024-10-11 13:34:23 +0330

    calculator

---
+++ m.js
@@ -0,0 +1,58 @@
+let display = document.getElementById('display');
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        calculate();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerText = currentOperand;
+}
